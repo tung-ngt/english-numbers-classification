8dfadc33822dc47f89c8d948b0154508e72261fa
NoVeTe36 <nguyenviettungntl3006@gmail.com>
2023-06-07 22:43:54 +0700

    Update audio-process to convert raw to processed, create spectrogram to perform the amplitude of frequency in time domain, and some minor changes

--- settings.py
+++ settings.py
@@ -1,4 +1,11 @@
 NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-NUMBERS_FOLDER = "./raw_audio"
+
+NUMBERS_FOLDER = "./raw_audio/"
+
 AUDIO_FORMAT = ".wav"
+
 PEOPLE = ["tt", "vt", "mt", "qs", "qh", "qt"]
+
+PROCESSED_FOLDER = "./processed_audio/"
+
+SPECTROGRAM_FOLDER = "./spectrogram/"
